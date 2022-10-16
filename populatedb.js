75d5ea4b69e40b694c3c5d47a7664659ad90838c
JosephR <joseph_rundell@outlook.com>
2022-10-16 02:13:10 -0400

    Basic test items created for database

---
+++ populatedb.js
@@ -0,0 +1,111 @@
+#! /usr/bin/env node
+
+console.log('This script populates some test items to our database.');
+
+// Get arguments passed on command line
+var userArgs = process.argv.slice(2);
+/*
+if (!userArgs[0].startsWith('mongodb')) {
+    console.log('ERROR: You need to specify a valid mongodb URL as the first argument');
+    return
+}
+*/
+const async = require('async');
+const User = require("./models/user");
+const Message = require("./models/message");
+const mongoose = require('mongoose');
+
+const mongoDB = userArgs[0];
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+let users = []
+let messages = []
+
+function userCreate(firstName, lastName, userName, password, membership, cb){
+    userdetail = {
+        firstName: firstName,
+        lastName: lastName,
+        userName: userName,
+        password: password,
+        membership: membership,
+    };
+
+    const user = new User(userdetail);
+
+    user.save(function(err){
+        if(err){
+            cb(err, null);
+            return;
+        }
+        console.log("New User: " + user);
+        users.push(user);
+        cb(null, user);
+    });
+}
+
+function messageCreate(messageText, postAuthor, datePosted, cb){
+    messagedetail = {
+        messageText: messageText,
+        datePosted: datePosted,
+    }
+    if(postAuthor != false) messagedetail.postAuthor = postAuthor
+
+    const message = new Message(messagedetail);
+
+    message.save(function(err){
+        if(err){
+            cb(err, null);
+            return;
+        }
+        console.log("New Message: " + message);
+        messages.push(message);
+        cb(null, message);
+    })
+}
+
+function createUsers(cb){
+    async.series([
+        function(callback){
+            userCreate("John", "Smith", "userSmith", "password2", "user", callback);
+        },
+        function(callback){
+            userCreate("Jane", "Doe", "userJane", "password2", "user", callback);
+        },
+        function(callback){
+            userCreate("Elizabeth", "Snow", "userSnow", "password2", "admin", callback);
+        }
+    ], cb)
+}
+
+function createMessages(cb){
+    async.series([
+        function(callback){
+            messageCreate("Hey!", [users[0]], Date.now(), callback);
+        },
+        function(callback){
+            messageCreate("Yo!", [users[1]], Date.now(), callback);
+        },
+    ], cb);
+};
+
+async.series([
+    createUsers,
+    createMessages,
+],
+// Optional callback
+function(err, results) {
+    if (err) {
+        console.log('FINAL ERR: '+err);
+    }
+    else {
+        console.log('Users: '+users);
+    }
+    // All done, disconnect from database
+    mongoose.connection.close();
+});
+
+
+
